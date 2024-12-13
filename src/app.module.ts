2c4c71b010b6fc84ecfb09b9597c60986dfaa60c
Alexandre Blanquero <aleaxndre.blanquero00@gmail.com>
2024-12-13 15:53:43 +0100

    feat: add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostController } from './post/post.controller';
+import { PostService } from './post/post.service';
+import { Post } from './post/post.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'pass',
+      database: 'docker_db',
+      autoLoadEntities: true,
+      retryAttempts: 5, 
+      retryDelay: 3000,
+      entities: [Post],
+      synchronize: true,
+    })
+  ],
+  controllers: [AppController, PostController],
+  providers: [AppService, PostService],
 })
 export class AppModule {}
